b2d446abbf8f10e870acf600cb7e151b023589f2
achutya1968 <39107934+achutya1968@users.noreply.github.com>
2022-02-23 13:37:42 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import { useFormik } from "formik";
+export default function App() {
+  const validate = (values) => {
+    const errors = {};
+    if (!values.email) {
+      errors.email = "Required valid email";
+    } else if (values.email.length < 4) {
+      errors.email = "Must be 5 characters or more";
+    }
+
+    if (!values.password) {
+      errors.password = "Required";
+    } else if (values.password.length < 8) {
+      errors.password = "Must be 8 characters or more";
+    } else if (values.password === "12345678") {
+      errors.password = "Can not be 12345678";
+    }
+
+    if (!values.confirmpassword) {
+      errors.confirmpassword = "Required";
+    } else if (values.confirmpassword !== values.password) {
+      errors.confirmpassword = "Password do not match";
+    }
+    return errors;
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+      confirmpassword: "",
+    },
+    validate,
+    onSubmit: (datas) => {
+      alert(JSON.stringify(datas, null, 2));
+    },
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Formik signup</h2>
+      <form className="form" onSubmit={formik.handleSubmit}>
+        <label htmlFor="email">Email:</label>
+        <br />
+        <input
+          type="email"
+          id="email"
+          name="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+          onBlur={formik.handleBlur}
+        />
+        <br />
+        {formik.touched.email && formik.errors.email ? (
+          <div className="err">{formik.errors.email}</div>
+        ) : null}
+
+        <label htmlFor="password">Password:</label>
+        <br />
+        <input
+          type="password"
+          id="password"
+          name="password"
+          onChange={formik.handleChange}
+          value={formik.values.password}
+          onBlur={formik.handleBlur}
+        />
+        <br />
+        {formik.touched.password && formik.errors.password ? (
+          <div className="err">{formik.errors.password}</div>
+        ) : null}
+
+        <label htmlFor="confirmpassword">Confirm password:</label>
+        <br />
+        <input
+          type="password"
+          id="confirmpassword"
+          name="confirmpassword"
+          onChange={formik.handleChange}
+          value={formik.values.confirmpassword}
+          onBlur={formik.handleBlur}
+        />
+        <br />
+        {formik.touched.confirmpassword && formik.errors.confirmpassword ? (
+          <div className="err">{formik.errors.confirmpassword}</div>
+        ) : null}
+
+        <button type="Submit">Submit</button>
+      </form>
     </div>
   );
 }
 
-export default App;
+
